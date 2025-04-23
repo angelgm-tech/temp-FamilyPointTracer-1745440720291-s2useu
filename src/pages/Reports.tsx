1db47fea1a4dd0965510abd3c26b44a41b9832bf
angelgm-tech <angelgm.tech@gmail.com>
2025-04-23 13:39:06 -0700

    Copy src/pages/Reports.tsx from FamilyPointTracer

---
+++ src/pages/Reports.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Reports = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Reports</h1>
+      {/* Reports content will be implemented later */}
+    </div>
+  );
+};
+
+export default Reports;
