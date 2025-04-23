f2bc193d75d67261a41ac6db03b2cbe10a0699d2
angelgm-tech <angelgm.tech@gmail.com>
2025-04-23 13:39:04 -0700

    Copy src/pages/Families.tsx from FamilyPointTracer

---
+++ src/pages/Families.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Families = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Families</h1>
+      {/* Families content will be implemented later */}
+    </div>
+  );
+};
+
+export default Families;
