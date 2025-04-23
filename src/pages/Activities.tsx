7e50e57e424b71c0d56c97505878a2e1c564e608
angelgm-tech <angelgm.tech@gmail.com>
2025-04-23 13:39:00 -0700

    Copy src/pages/Activities.tsx from FamilyPointTracer

---
+++ src/pages/Activities.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Activities = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Activities</h1>
+      {/* Activities content will be implemented later */}
+    </div>
+  );
+};
+
+export default Activities;
